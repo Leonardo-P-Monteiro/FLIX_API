f6eb5977cbebf2ea7991485ed54d20557395ce79
Leonardo P Monteiro <dev.pmonteiro@gmail.com>
2025-07-14 09:55:27 -0300

    ATT

--- genres/views.py
+++ genres/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from genres.models import Genre
 
 # Create your views here.
+def genre_view(request):
+    """
+    Consulta os gêneros existentes no banco de dados.
+    """
+    genres = Genre.objects.all()
+    genres_dict = list(genres.values())
+    data = [{'id':genre.id,'name':genre.name} for genre in genres]
+    
+    return JsonResponse(genres_dict, safe=False)
+
+
+
+
