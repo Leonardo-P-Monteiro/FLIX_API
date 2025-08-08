f0a56f7dd16247727c802776061c9a469dd813ad
Leonardo P Monteiro <dev.pmonteiro@gmail.com>
2025-08-08 16:45:00 -0300

    att

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,9 +1,34 @@
 from rest_framework import serializers
 from movies.models import Movie
+from django.db.models import Avg
 
 
-class MovieSerializer(serializers.ModelSerializer):
+class MovieModelSerializer(serializers.ModelSerializer):
+    rate = serializers.SerializerMethodField(read_only = True)
 
     class Meta:
         model = Movie
         fields = '__all__'
+    
+    def get_rate(self, obj):
+        
+        rate = obj.reviews.aggregate(Avg('stars'))
+        result = rate['stars__avg']
+
+        if result: 
+            return round(result, 2)
+        else:
+            return None
+
+    def validate_release_date(self, value):
+        if value.year < 1_900:
+            raise serializers.ValidationError('A data de lançamento não pode \
+ser inferior a 1990.')
+        return value
+    
+    def validate_resume(self, value):
+        if len(value) > 1_000:
+            raise serializers.ValidationError('O resumo do filme não pode ser \
+superior a 200 caracteres.')
+        
+        return value
